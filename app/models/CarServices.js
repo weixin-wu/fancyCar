83344c007346a4923f77eddbf7f063684e63b8b5
wuweixin <wuweixin6@gmail.com>
2018-05-13 19:02:43 -0400

    removed mongo

--- app/models/CarServices.js
+++ app/models/CarServices.js
@@ -6,22 +6,25 @@ var url =
   "mongodb://mongodb-stitch-storage-gyasn:Ag0JTRrjNwXsccsf@cluster0-shard-00-00-v1bxg.mongodb.net:27017,cluster0-shard-00-01-v1bxg.mongodb.net:27017,cluster0-shard-00-02-v1bxg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
 exports.getAllCar = function (cb) {
-  MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
-    assert.equal(null, err);
-    const db = client.db("test");
-    db
-      .collection("cars")
-      .find({})
-      .toArray(function (err, results) {
-        var parsedResult = results.map(data => {
-          delete data["_id"];
+  // MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+  //   assert.equal(null, err);
+  //   const db = client.db("test");
+  //   db
+  //     .collection("cars")
+  //     .find({})
+  //     .toArray(function (err, results) {
+  //       var parsedResult = results.map(data => {
+  //         delete data["_id"];
 
-          return data;
-        });
-        cb(parsedResult);
-      });
-    client.close();
-  });
+  //         return data;
+  //       });
+  //       cb(parsedResult);
+  //     });
+  //   client.close();
+  // });
+
+  
+  cb(allCars.allCars);
 };
 
 exports.getAvailability = function (id, cb) {
